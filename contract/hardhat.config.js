a04edabf46ba63ef959e24822c8970eb81f315ac
le1205 <legend.dev.goD@outlook.com>
2024-07-18 17:12:05 +0700

    Danh Tran's commit for technical assessment

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -1,14 +1,13 @@
-require('dotenv').config();
+require("dotenv").config();
 
-require('@nomicfoundation/hardhat-toolbox');
-require('hardhat-gas-reporter');
-require('hardhat-contract-sizer');
-require('solidity-coverage');
-
+require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-gas-reporter");
+require("hardhat-contract-sizer");
+require("solidity-coverage");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: '0.8.20',
+  solidity: "0.8.20",
   settings: {
     optimizer: {
       enabled: true,
@@ -27,26 +26,29 @@ module.exports = {
     //   gasPrice: 15000000000,
     // },
     defaultNetwork: {
-      url: 'hardhat',
+      url: "hardhat",
     },
     hardhat: {
       allowUnlimitedContractSize: true,
     },
     eth_sepolia: {
-      url: 'https://ethereum-sepolia.blockpi.network/v1/rpc/public',
-      accounts: ['xxxx'], // Replace by privateKey
+      url: "https://ethereum-sepolia.blockpi.network/v1/rpc/public",
+      accounts: [
+        "d0bb929f04a5ade527bf484fc9c53bb35463ab2d34f766839968f217cc25ef10",
+      ], // Replace by privateKey
       // gasPrice: 15000000000,
     },
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
-    currency: 'USD',
-    token: 'ETH',
-    gasPriceApi: 'https://api.etherscan.io/api?module=proxy&action=eth_gasPrice',
-    coinmarketcap: '1bdef299-9f72-465d-a680-35fdb0b59db0',
+    currency: "USD",
+    token: "ETH",
+    gasPriceApi:
+      "https://api.etherscan.io/api?module=proxy&action=eth_gasPrice",
+    coinmarketcap: "1bdef299-9f72-465d-a680-35fdb0b59db0",
   },
   etherscan: {
-    apiKey: 'K8JNR94ITH2XCRX6V15P8D483D2FM6C99Y',
+    apiKey: "K8JNR94ITH2XCRX6V15P8D483D2FM6C99Y",
     // apiKey: '2JSFYE3G4DEMNFY5X1C34H28XNVUKGWWJ4',
     // apiKey: {
     //   // mantle_testnet: 'xyz', //random value
